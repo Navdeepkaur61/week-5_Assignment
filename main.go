a3125a1d5170d1ec1268273c01b95e302944a870
suraiya-jabeen <suraiyajabeen@loyalistcollege.com>
2024-05-30 20:18:20 -0400

    commit by Suraiya

--- main.go
+++ main.go
@@ -9,16 +9,18 @@ import (
 )
 
 func main() {
+	// Creating a new scanner to read from console input
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Println("Enter a temperature (32 F or 100 C): or type y to quit ")
-		scanner.Scan()
+		fmt.Println("Enter a temperature (32 F or 100 C): or type q to quit ")
+		scanner.Scan() //read user input
 
-		temperature := scanner.Text()
+		temperature := scanner.Text() // user input is stored as a string
 
+		//strings.Fields splits the user input into temperature value and unit of temperatur based on white space
 		splitInput := strings.Fields(temperature)
-		if strings.ToLower(temperature) == "y" {
+		if strings.ToLower(temperature) == "q" {
 			break
 		}
 
@@ -28,8 +30,10 @@ func main() {
 			continue
 		}
 
+		// get the unit of temperture
 		unitOfTemperture := strings.ToUpper(splitInput[1])
 
+		//convert the string (temperature value) to a floating-point number
 		tempertureToConvert, err := strconv.ParseFloat(splitInput[0], 64)
 
 		if err != nil {
@@ -37,11 +41,14 @@ func main() {
 			continue
 		}
 
+		// using if statement to check the unit of the temperture
 		if unitOfTemperture == "F" {
+			// Calling FahrenheitToCelsius to convert fahrenheit into celsius
 			celsius := FahrenheitToCelsius(tempertureToConvert)
 			fmt.Printf("Your Celsius value is %.2f C\n", celsius)
 		} else if unitOfTemperture == "C" {
 			fahrenheit := CelsiusToFahrenheit(tempertureToConvert)
+			// Calling FahrenheitToCelsius to convert celsius into fahrenheit
 			fmt.Printf("Your Fahrenheit value is %.2f F\n", fahrenheit)
 		} else {
 			fmt.Println("Invalid unit. Please use 'F' for Fahrenheit or 'C' for Celsius.")
@@ -50,10 +57,12 @@ func main() {
 	}
 }
 
+// Function to convert fahrenheit into celsius
 func FahrenheitToCelsius(f float64) float64 {
 	return (f - 32) * 5 / 9
 }
 
+// Function to convert celsius into fahrenheit
 func CelsiusToFahrenheit(c float64) float64 {
 	return c*9/5 + 32
 }
